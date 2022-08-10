bbb4031e8762845d1865c73dfb3c6ebc7f35593e
tesfayealex <tesfayealemayehu27@gmail.com>
2022-08-10 03:26:17 +0300

    successful run dbt and commit logs

---
+++ dbt/target/compiled/ad/models/advert/cleaner_model.sql
@@ -0,0 +1,4 @@
+
+select *
+from "trial"."public"."merger_model"
+where row_num = 1
---
+++ dbt/target/run/ad/models/advert/merger_model.sql
@@ -0,0 +1,22 @@
+
+
+  create  table "trial"."public"."merger_model__dbt_tmp"
+  as (
+    
+
+with tempbriefing as (
+    SELECT 
+       *,
+        ROW_NUMBER() OVER (
+            PARTITION BY 
+               "campaign_id"
+            ORDER BY 
+               "campaign_id"
+        ) "row_num"
+     FROM 
+        "trial"."public"."briefing"
+        )
+
+-- create table if not exists public.warehouse as 
+SELECT * FROM "trial"."public"."campaigns_inventory" left join "trial"."public"."design" using ("game_key") left join tempbriefing using ("campaign_id")
+  );
